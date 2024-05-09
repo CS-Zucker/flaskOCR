d61fea41479f8de5baec5a4b2939586b75fcb6f9
Likkas <327084701@qq.com>
2024-05-09 15:58:43 +0800

    二维码，银行卡，数字，手写的orc视图文件

--- pear_admin/views/general_OCR.py
+++ pear_admin/views/general_OCR.py
@@ -11,3 +11,22 @@ def general_OCR_word():
 def general_OCR_seal():
     return render_template(f"view/general_OCR/OCR_seal.html")
 
+
+@general_OCR_bp.get("/general_OCR/qrcode")
+def general_OCR_seal():
+    return render_template(f"view/general_OCR/OCR_seal.html")
+
+
+@general_OCR_bp.get("/general_OCR/handwriting")
+def general_OCR_seal():
+    return render_template(f"view/general_OCR/OCR_seal.html")
+
+
+@general_OCR_bp.get("/general_OCR/number")
+def general_OCR_seal():
+    return render_template(f"view/general_OCR/OCR_seal.html")
+
+
+@general_OCR_bp.get("/general_OCR/bankcard")
+def general_OCR_seal():
+    return render_template(f"view/general_OCR/OCR_seal.html")
